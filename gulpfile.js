55161c808f53fb53c3965ae2661bafe72f796b25
Eric <eric@pin12.nl>
2018-01-14 10:18:19 +0100

    Aanvullingen

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
+var watch = require('gulp-watch'); 
+var postcss = require('gulp-postcss'); 
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars'); // Adds support for variables in css
+var cssNested = require('postcss-nested'); // Adds support for nested css 
+var cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log('Default task is called and run.');
@@ -10,7 +15,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-    console.log('Styles task is called and run');
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, cssNested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
